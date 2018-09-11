34bdd4f31c89693bad9bb6ea90db6c96d2fe2a07
Dali Gala <habaresbit@gmail.com>
2018-09-11 02:41:29 -0400

    new project created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple McDouble", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese & Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Smokehouse Burger", true);
